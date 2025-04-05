eaa4a34295b5336b3a95ffa8f4cb11a707251bb2
jcsilvaaa <162688556+jcsilvaaa@users.noreply.github.com>
2025-04-05 19:36:06 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/uploads", express.static("uploads")); 
+app.use("/images", express.static(path.join(__dirname, "Public", "images")));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Serve homepage.html for the root route
 app.get("/homepage", (req, res) => {
@@ -221,6 +224,4 @@ app.delete("/reviews/:branch/:id", async (req, res) => {
 // Start Server
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
 
-app.use("/images", express.static(path.join(__dirname, "Public", "images")));
-app.use(express.static(path.join(__dirname, 'public')));
 
